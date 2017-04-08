9371ab0895924f73d97b4ec2ce06d2bd3c5ba649
mtezych <mte.zych@gmail.com>
2017-04-08 16:42:22 +0200

    Vulkan -> Added Image object #2.

---
+++ vulkan/Image.h
@@ -0,0 +1,36 @@
+
+#ifndef VULKAN_IMAGE
+#define VULKAN_IMAGE
+
+#define VK_NO_PROTOTYPES
+#include <vulkan/vulkan.h>
+
+namespace vk
+{
+	struct Device;
+
+	struct Image
+	{
+		const Device* device;
+		VkImage       vkImage;
+		bool          isWSI;
+
+		Image (const Device& device, const VkImage image);
+
+		Image
+		(
+			const Device&            device,
+			const VkImageCreateInfo& createInfo
+		);
+
+		~Image ();
+
+		Image (Image&& image);
+		Image (const Image& image) = delete;
+
+		Image& operator = (Image&& image);
+		Image& operator = (const Image& image) = delete;
+	};
+}
+
+#endif
