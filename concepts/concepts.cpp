3730ffd533f6ab45e78b43bf036a12051b8fae58
mtezych <mte.zych@gmail.com>
2018-09-16 23:56:59 +0200

    Concepts -> Renamed to concepts, moved to C++17 and removed all W/As.

--- concepts/concepts.cpp
+++ concepts/concepts.cpp
@@ -32,22 +32,25 @@
  * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+// Constraints and concepts
+// ~ http://en.cppreference.com/w/cpp/language/constraints
+
 #include <utility>
 #include <type_traits>
 #include <cstdint>
-#include <array>
 
-namespace foo
+namespace concepts
 {
 	template <typename Type>
-	constexpr bool Moveable()
+	constexpr bool Moveable ()
 	{
-		return	std::is_move_constructible<Type>::value &&
-				std::is_move_assignable   <Type>::value;
+		return
+		std::is_move_constructible<Type>::value &&
+		std::is_move_assignable   <Type>::value;
 	}
 
 	template <typename Type>
-	constexpr bool Destructible()
+	constexpr bool Destructible ()
 	{
 		return std::is_destructible<Type>::value;
 	}
@@ -55,16 +58,18 @@ namespace foo
 	template <typename Type> requires Moveable<Type>() && Destructible<Type>()
 	void swap (Type& left, Type& right)
 	{
-		auto temp = std::move(left);  // move constructor
-		left      = std::move(right); // move assign operator
+		auto temp = std::move(left);
+		left      = std::move(right);
 		right     = std::move(temp);
 	}
 
+
 	template <typename Type>
-	concept constexpr bool Copyable()
+	concept constexpr bool Copyable ()
 	{
-		return	std::is_copy_constructible<Type>::value &&
-				std::is_copy_assignable   <Type>::value;
+		return
+		std::is_copy_constructible<Type>::value &&
+		std::is_copy_assignable   <Type>::value;
 	}
 
 	template <typename Type>
@@ -82,7 +87,8 @@ namespace foo
 	Type max (const Type& left, const Type& right)
 	{
 		static_assert(Copyable<Type>(), "Type has to be Copyable.");
-		if (true)
+
+		if constexpr (true)
 		{
 			return (left > right) ? left : right; //   less than & copy
 		}
@@ -92,20 +98,22 @@ namespace foo
 		}
 	}
 
+
 	template <typename Type, typename... ArgumentTypes>
 	concept bool Callable = requires (Type callable, ArgumentTypes... arguments)
 	{
 		{ callable(arguments...) };
 	};
 
-	template <typename CallableType, typename... ArgumentTypes> 
-	requires Callable<CallableType, ArgumentTypes...> 
-	auto invoke(const CallableType& callable, ArgumentTypes&&... arguments)
+	template <typename CallableType, typename... ArgumentTypes>
+	requires Callable<CallableType, ArgumentTypes...>
+	auto invoke (const CallableType& callable, ArgumentTypes&&... arguments)
 	{
 		return callable(std::forward<ArgumentTypes>(arguments)...);
 	}
 
-	template <uint8_t Element, uint8_t... Elements> // concepts not needed here
+
+	template <auto Element, auto... Elements>
 	struct reduce
 	{
 		static_assert
@@ -117,10 +125,10 @@ namespace foo
 		constexpr static auto value = Element + reduce<Elements...>::value;
 	};
 
-	template <uint8_t Element>
-	struct reduce<Element>
+	template <auto Element>
+	struct reduce <Element>
 	{
-		constexpr static auto value = Element; 
+		constexpr static auto value = Element;
 	};
 
 	template <typename Type>
@@ -144,32 +152,19 @@ namespace foo
 		{ Type { 0 } };
 	};
 
-	// WA: Allows handling empty parameter pack.
-	constexpr auto accumulate ()
-	{
-		return 0;
-	}
-
-	// WA: This overload is not called.
-	template <Arithmetic ElementType>
-	constexpr auto accumulate (ElementType element)
-	{
-		return element;
-	}
-
 	template <Arithmetic ElementType, Arithmetic... ElementTypes>
 	constexpr auto accumulate (ElementType element, ElementTypes... elements)
 	{
-		// WA: Overload resolution fails to choose overload with one element.
-		//     Because of that reason empty parameter pack is generated.
-		// static_assert
-		// (
-		// 	sizeof...(elements) > 0,
-		// 	"Empty parameter pack should not happen. "
-		// 	"Overload with one element should be preffered."
-		// );
-		return element + accumulate(elements...);
+		if constexpr (sizeof...(elements) > 0)
+		{
+			return element + accumulate(elements...);
+		}
+		else
+		{
+			return element;
+		}
 	}
+
 
 	template <typename Type>
 	concept bool Iterator = requires (Type iterator, Type other)
@@ -188,7 +183,7 @@ namespace foo
 		{ container.end()   } -> typename Type::iterator;
 	};
 
-	void sort(Container& container)
+	void sort (Container& container)
 	{
 		for (auto& elementA : container)
 		{
@@ -197,7 +192,7 @@ namespace foo
 				static_assert
 				(
 					Comparable<decltype(elementA)>,
-					"Container's Elements have to be Comparable."
+					"Containers' Elements have to be Comparable."
 				);
 				if (elementA < elementB)
 				{
@@ -208,58 +203,86 @@ namespace foo
 	}
 }
 
+
 namespace bar
 {
 	struct Bar
 	{
-		uint32_t b;
+		uint32_t value;
 
-		bool operator <  (const Bar& bar) const { return this->b <  bar.b; }
-		bool operator >  (const Bar& bar) const { return this->b >  bar.b; }
-		bool operator <= (const Bar& bar) const { return this->b <= bar.b; }
-		bool operator >= (const Bar& bar) const { return this->b >= bar.b; }
-		bool operator == (const Bar& bar) const { return this->b == bar.b; }
-		bool operator != (const Bar& bar) const { return this->b != bar.b; }
+		bool operator <  (const Bar& bar) const { return value <  bar.value; }
+		bool operator >  (const Bar& bar) const { return value >  bar.value; }
+		bool operator <= (const Bar& bar) const { return value <= bar.value; }
+		bool operator >= (const Bar& bar) const { return value >= bar.value; }
+		bool operator == (const Bar& bar) const { return value == bar.value; }
+		bool operator != (const Bar& bar) const { return value != bar.value; }
 	};
-}
 
-namespace foo
-{
-	// WA: Explicit (full) template specialization does not work.
-	//     Is overload resolution failing because
-	//     template requirements are part of the interface?
-	// template <>
-	void swap /* <bar::Bar> */ (bar::Bar& left, bar::Bar& right)
+	void swap (bar::Bar& left, bar::Bar& right)
 	{
-		left .b = left.b ^ right.b;
-		right.b = left.b ^ right.b;
-		left .b = left.b ^ right.b;
+		left .value = left.value ^ right.value;
+		right.value = left.value ^ right.value;
+		left .value = left.value ^ right.value;
 	}
 }
 
+
+#include <array>
+#include <cassert>
+
 int main()
 {
-	auto x = 1;
-	auto y = 2;
-	foo::swap(x, y);
-	auto p = bar::Bar { 47 };
-	auto q = bar::Bar { 31 };
-	foo::swap(p, q);
-	
-	const auto a = foo::max(2, 4);
-	const auto b = foo::max(1.2f, 1.3f);
-	const auto c = foo::max(bar::Bar { 54 }, bar::Bar { 34 });
+	{
+		using concepts::swap;
 
-	const auto l = [](){};
-	foo::invoke(l);
-	const auto m = [](int, char) -> double { return 5.6; };
-	const double d = foo::invoke(m, 45, '#');
+		auto x = 1;
+		auto y = 2;
+		swap(x, y);
+		assert(x == 2);
+		assert(y == 1);
 
-	constexpr auto s = foo::reduce<8, 6, 2, 7>::value;
-	constexpr auto t = foo::accumulate(8, 6, 2, 7);
+		auto a = bar::Bar { 47 };
+		auto b = bar::Bar { 31 };
+		swap(a, b);
+		assert(a == bar::Bar { 31 });
+		assert(b == bar::Bar { 47 });
+	}
 
-	auto v = std::array<bar::Bar, 5> { 123, 53, 421, 7, 112 };
-	foo::sort(v);
+	{
+		const auto i = concepts::max(2, 4);
+		assert(i == 4);
+
+		const auto f = concepts::max(1.2f, 1.3f);
+		assert(f == 1.3f);
+
+		const auto b = concepts::max(bar::Bar { 54 }, bar::Bar { 34 });
+		assert(b == bar::Bar { 54 });
+	}
+
+	{
+		const auto nop = [](){};
+		concepts::invoke(nop);
+
+		const auto lambda = [] (int, char) { return 5.6; };
+		const auto d = double
+		{
+			concepts::invoke(lambda, 45, '#')
+		};
+		assert(d == 5.6);
+	}
+
+	{
+		constexpr auto r = concepts::reduce<8, 6, 2, 7>::value;
+		assert(r == 23);
+
+		constexpr auto a = concepts::accumulate(8, 6, 2, 7);
+		assert(a == 23);
+	}
+
+	{
+		auto array = std::array<bar::Bar, 5> { 123, 53, 421, 7, 112 };
+		concepts::sort(array);
+	}
 
 	return 0;
 }
